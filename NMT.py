e92bb91c288e2f1d768f3d455405ffa3fa5d1b7c
Salih Gundogdu <salihgundogdu@Salihs-MacBook-Pro.local>
2018-11-30 19:15:36 -0500

    python argv

---
+++ NMT.py
@@ -0,0 +1,275 @@
+import tensorflow as tf
+import numpy as np
+import time, os, random, sys
+import data
+import config
+
+
+class TranslationModel:
+    def __init__(self, forward_only, batch_size):
+        """forward_only: if set, we do not construct the backward pass in the model.
+        """
+        print('Initialize new model')
+        self.fw_only = forward_only
+        self.batch_size = batch_size
+
+    def _create_placeholders(self):
+        # Feeds for inputs. It's a list of placeholders
+        print('Create placeholders')
+        self.encoder_inputs = [tf.placeholder(tf.int32, shape=[None], name='encoder{}'.format(i))
+                               for i in range(config.BUCKETS[-1][0])]
+        self.decoder_inputs = [tf.placeholder(tf.int32, shape=[None], name='decoder{}'.format(i))
+                               for i in range(config.BUCKETS[-1][1] + 1)]
+        self.decoder_masks = [tf.placeholder(tf.float32, shape=[None], name='mask{}'.format(i))
+                              for i in range(config.BUCKETS[-1][1] + 1)]
+
+        # Our targets are decoder inputs shifted by one (to ignore <GO> symbol)
+        self.targets = self.decoder_inputs[1:]
+
+    def _inference(self):
+        print('Create inference')
+        # If we use sampled softmax, we need an output projection.
+        # Sampled softmax only makes sense if we sample less than vocabulary size.
+        if config.NUM_SAMPLES > 0 and config.NUM_SAMPLES < config.DEC_VOCAB:
+            w = tf.get_variable('proj_w', [config.HIDDEN_SIZE, config.DEC_VOCAB])
+            b = tf.get_variable('proj_b', [config.DEC_VOCAB])
+            self.output_projection = (w, b)
+
+        def sampled_loss(logits, labels):
+            labels = tf.reshape(labels, [-1, 1])
+            return tf.nn.sampled_softmax_loss(weights=tf.transpose(w), 
+                                              biases=b, 
+                                              inputs=logits, 
+                                              labels=labels, 
+                                              num_sampled=config.NUM_SAMPLES, 
+                                              num_classes=config.DEC_VOCAB)
+        self.softmax_loss_function = sampled_loss
+
+        single_cell = tf.contrib.rnn.GRUCell(config.HIDDEN_SIZE)
+        self.cell = tf.contrib.rnn.MultiRNNCell([single_cell for _ in range(config.NUM_LAYERS)])
+
+    def _create_loss(self):
+        print('Creating loss... \nIt might take a couple of minutes depending on how many buckets you have.')
+        start = time.time()
+        def _seq2seq_f(encoder_inputs, decoder_inputs, do_decode):
+            setattr(tf.contrib.rnn.GRUCell, '__deepcopy__', lambda self, _: self)
+            setattr(tf.contrib.rnn.MultiRNNCell, '__deepcopy__', lambda self, _: self)
+            return tf.contrib.legacy_seq2seq.embedding_attention_seq2seq(
+                    encoder_inputs, decoder_inputs, self.cell,
+                    num_encoder_symbols=config.ENC_VOCAB,
+                    num_decoder_symbols=config.DEC_VOCAB,
+                    embedding_size=config.HIDDEN_SIZE,
+                    output_projection=self.output_projection,
+                    feed_previous=do_decode)
+
+        if self.fw_only:
+            self.outputs, self.losses = tf.contrib.legacy_seq2seq.model_with_buckets(
+                                        self.encoder_inputs, 
+                                        self.decoder_inputs, 
+                                        self.targets,
+                                        self.decoder_masks, 
+                                        config.BUCKETS, 
+                                        lambda x, y: _seq2seq_f(x, y, True),
+                                        softmax_loss_function=self.softmax_loss_function)
+            # If we use output projection, we need to project outputs for decoding.
+            if self.output_projection:
+                for bucket in range(len(config.BUCKETS)):
+                    self.outputs[bucket] = [tf.matmul(output, 
+                                            self.output_projection[0]) + self.output_projection[1]
+                                            for output in self.outputs[bucket]]
+        else:
+            self.outputs, self.losses = tf.contrib.legacy_seq2seq.model_with_buckets(
+                                        self.encoder_inputs, 
+                                        self.decoder_inputs, 
+                                        self.targets,
+                                        self.decoder_masks,
+                                        config.BUCKETS,
+                                        lambda x, y: _seq2seq_f(x, y, False),
+                                        softmax_loss_function=self.softmax_loss_function)
+        print('Time:', time.time() - start)
+
+    def _creat_optimizer(self):
+        print('Create optimizer... \nIt might take a couple of minutes depending on how many buckets you have.')
+        with tf.variable_scope('training') as scope:
+            self.global_step = tf.Variable(0, dtype=tf.int32, trainable=False, name='global_step')
+
+            if not self.fw_only:
+                self.optimizer = tf.train.GradientDescentOptimizer(config.LR)
+                trainables = tf.trainable_variables()
+                self.gradient_norms = []
+                self.train_ops = []
+                start = time.time()
+                for bucket in range(len(config.BUCKETS)):
+                    
+                    clipped_grads, norm = tf.clip_by_global_norm(tf.gradients(self.losses[bucket], 
+                                                                 trainables),
+                                                                 config.MAX_GRAD_NORM)
+                    self.gradient_norms.append(norm)
+                    self.train_ops.append(self.optimizer.apply_gradients(zip(clipped_grads, trainables), 
+                                                            global_step=self.global_step))
+                    print('Creating opt for bucket {} took {} seconds'.format(bucket, time.time() - start))
+                    start = time.time()
+
+
+    def _create_summary(self):
+        pass
+
+    def build_graph(self):
+        self._create_placeholders()
+        self._inference()
+        self._create_loss()
+        self._creat_optimizer()
+        self._create_summary()
+
+
+def _get_random_bucket(train_buckets_scale):
+    """ Get a random bucket from which to choose a training sample """
+    rand = random.random()
+    return min([i for i in range(len(train_buckets_scale))
+                if train_buckets_scale[i] > rand])
+
+def _assert_lengths(encoder_size, decoder_size, encoder_inputs, decoder_inputs, decoder_masks):
+    """ Assert that the encoder inputs, decoder inputs, and decoder masks are
+    of the expected lengths """
+    if len(encoder_inputs) != encoder_size:
+        raise ValueError("Encoder length must be equal to the one in bucket,"
+                        " %d != %d." % (len(encoder_inputs), encoder_size))
+    if len(decoder_inputs) != decoder_size:
+        raise ValueError("Decoder length must be equal to the one in bucket,"
+                       " %d != %d." % (len(decoder_inputs), decoder_size))
+    if len(decoder_masks) != decoder_size:
+        raise ValueError("Weights length must be equal to the one in bucket,"
+                       " %d != %d." % (len(decoder_masks), decoder_size))
+
+
+def run_step(sess, model, encoder_inputs, decoder_inputs, decoder_masks, bucket_id, forward_only):
+    """ Run one step in training.
+    @forward_only: boolean value to decide whether a backward path should be created
+    forward_only is set to True when you just want to evaluate on the test set,
+    or when you want to the bot to be in translation mode. """
+    encoder_size, decoder_size = config.BUCKETS[bucket_id]
+    _assert_lengths(encoder_size, decoder_size, encoder_inputs, decoder_inputs, decoder_masks)
+
+    # input feed: encoder inputs, decoder inputs, target_weights, as provided.
+    input_feed = {}
+    for step in range(encoder_size):
+        input_feed[model.encoder_inputs[step].name] = encoder_inputs[step]
+    for step in range(decoder_size):
+        input_feed[model.decoder_inputs[step].name] = decoder_inputs[step]
+        input_feed[model.decoder_masks[step].name] = decoder_masks[step]
+
+    last_target = model.decoder_inputs[decoder_size].name
+    input_feed[last_target] = np.zeros([model.batch_size], dtype=np.int32)
+
+    # output feed: depends on whether we do a backward step or not.
+    if not forward_only:
+        output_feed = [model.train_ops[bucket_id],  # update op that does SGD.
+                       model.gradient_norms[bucket_id],  # gradient norm.
+                       model.losses[bucket_id]]  # loss for this batch.
+    else:
+        output_feed = [model.losses[bucket_id]]  # loss for this batch.
+        for step in range(decoder_size):  # output logits.
+            output_feed.append(model.outputs[bucket_id][step])
+
+    outputs = sess.run(output_feed, input_feed)
+    if not forward_only:
+        return outputs[1], outputs[2], None  # Gradient norm, loss, no outputs.
+    else:
+        return None, outputs[0], outputs[1:]  # No gradient norm, loss, outputs.
+
+def _check_restore_parameters(sess, saver):
+    """ Restore the previously trained parameters if there are any. """
+    ckpt = tf.train.get_checkpoint_state(os.path.dirname(config.CPT_PATH + '/checkpoint'))
+    if ckpt and ckpt.model_checkpoint_path:
+        print("Loading parameters for the TranslationModel")
+        saver.restore(sess, ckpt.model_checkpoint_path)
+    else:
+        print("Initializing fresh parameters for the TranslationModel")
+
+def _get_random_bucket(train_buckets_scale):
+    """ Get a random bucket from which to choose a training sample """
+    rand = random.random()
+    return min([i for i in range(len(train_buckets_scale))
+                if train_buckets_scale[i] > rand])
+
+def _get_skip_step(iteration):
+    """ How many steps should the model train before it saves all the weights. """
+    if iteration < 100:
+        return 30
+    return 100
+
+def _get_buckets():
+    """ Load the dataset into buckets based on their lengths.
+    train_buckets_scale is the inverval that'll help us 
+    choose a random bucket later on.
+    """
+    test_buckets = data.load_data('tst2012_ids.en', 'tst2012_ids.vi')
+    data_buckets = data.load_data('train_ids.en', 'train_ids.vi')
+    train_bucket_sizes = [len(data_buckets[b]) for b in range(len(config.BUCKETS))]
+    print("Number of samples in each bucket:\n", train_bucket_sizes)
+    train_total_size = sum(train_bucket_sizes)
+    # list of increasing numbers from 0 to 1 that we'll use to select a bucket.
+    train_buckets_scale = [sum(train_bucket_sizes[:i + 1]) / train_total_size
+                           for i in range(len(train_bucket_sizes))]
+    print("Bucket scale:\n", train_buckets_scale)
+    return test_buckets, data_buckets, train_buckets_scale
+
+
+def train():
+    data.token2id('train', 'en') #encode
+    data.token2id('train', 'vi') # decode
+    data.token2id('tst2012', 'en')
+    data.token2id('tst2012', 'vi')
+
+    """ Train the NMT """
+    test_buckets, data_buckets, train_buckets_scale = _get_buckets()
+    # in train mode, we need to create the backward path, so forwrad_only is False
+    model = TranslationModel(False, config.BATCH_SIZE)
+    model.build_graph()
+
+    saver = tf.train.Saver()
+
+    with tf.Session() as sess:
+            print('Running session')
+            sess.run(tf.global_variables_initializer())
+            _check_restore_parameters(sess, saver)
+
+            iteration = model.global_step.eval()
+            total_loss = 0
+            while True:
+                skip_step = _get_skip_step(iteration)
+                bucket_id = _get_random_bucket(train_buckets_scale)
+                encoder_inputs, decoder_inputs, decoder_masks = data.get_batch(data_buckets[bucket_id], 
+                                                                               bucket_id,
+                                                                               batch_size=config.BATCH_SIZE)
+                start = time.time()
+                _, step_loss, _ = run_step(sess, model, encoder_inputs, decoder_inputs, decoder_masks, bucket_id, False)
+                total_loss += step_loss
+                iteration += 1
+                
+                if iteration % skip_step == 0:
+                    print('Iter {}: loss {}, time {}'.format(iteration, total_loss/skip_step, time.time() - start))
+                    start = time.time()
+                    total_loss = 0
+                    saver.save(sess, os.path.join(config.CPT_PATH, 'TranslationModel'), global_step=model.global_step)
+                    if iteration % (10 * skip_step) == 0:
+                        # Run evals on development set and print their loss
+                        _eval_test_set(sess, model, test_buckets)
+                        start = time.time()
+                    sys.stdout.flush()
+
+def main():
+    run_type = sys.argv[1]
+    if run_type == 'train':
+        train()
+    elif run_type == 'test':
+        pass
+    elif run_type == 'translate':
+        pass
+    else: raise ValueError('You should input either `train`, `test` or `translate`!')
+
+
+
+
+if __name__ == '__main__':
+    main()
